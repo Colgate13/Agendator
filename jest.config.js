479ed8831e23e67d64e4504441f9be222586d439
Colgate13 <gabreilbarros13@gmail.com>
2022-08-19 11:13:33 -0300

    Setting project and 100% Test unit
    Setting project and 100% Test unit

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
   clearMocks: true,
-  collectCoverage: true,
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/I*.ts',
@@ -8,18 +11,51 @@ module.exports = {
     '!src/**/config/*.ts',
     '!src/infra/**/*.ts',
     '!src/core/**/*.ts',
+    '!src/shared/Error/*',
+    '!src/server.ts'
   ],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  testMatch: [
+    '**/*.spec.ts',
+    '**/*.test.ts',
+  ],
   coverageReporters: [
     'text-summary',
     'lcov',
     'html'
   ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src',
+  }),
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    '**/*.spec.ts',
-    '**/*.test.ts',
-  ]
-};
+}
+
+// import { pathsToModuleNameMapper } from 'ts-jest/utils'
+// import { compilerOptions } from './tsconfig.json'
+
+// export default {
+//   clearMocks: true,
+//   coverageDirectory: 'coverage',
+//   testEnvironment: 'node',
+//   collectCoverageFrom: [
+//     'src/**/*.ts',
+//     '!src/**/I*.ts',
+//     '!src/**/@types/*.ts',
+//     '!src/**/config/*.ts',
+//     '!src/infra/**/*.ts',
+//     '!src/core/**/*.ts',
+//   ],
+//   testMatch: [
+//     '**/*.spec.ts',
+//     '**/*.test.ts',
+//   ],
+//   coverageReporters: [
+//     'text-summary',
+//     'lcov',
+//     'html'
+//   ],
+//   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+//     prefix: '<rootDir>/src',
+//   }),
+//   preset: 'ts-jest',
+// }
+
