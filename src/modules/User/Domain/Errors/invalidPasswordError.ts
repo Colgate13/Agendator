ffc424f16bea72b20c8a5f6ac4ada832475dd3d0
Colgate13 <gabreilbarros13@gmail.com>
2022-09-15 16:00:46 -0300

    Update

--- src/modules/User/Domain/Errors/invalidPasswordError.ts
+++ src/modules/User/Domain/Errors/invalidPasswordError.ts
@@ -1,8 +1,8 @@
 import { CoreError } from '../../../../shared/Error/CoreError';
 
-export class InvalidPassword extends CoreError {
+export class InvalidPasswordError extends CoreError {
   constructor() {
     super('The password must have between 6 and 255 characters.');
-    this.name = 'InvalidPassword';
+    this.name = 'InvalidPasswordError';
   }
 }
